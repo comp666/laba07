d3512c3b889c7b085b300097edafb1535b3474cb
comp666 <comp622012016@mail.ru>
2016-09-30 15:23:22 +0300

    first commit

--- index.php
+++ index.php
@@ -3,3 +3,10 @@ echo date("l ds of F Y h:s A")."<br>";
 echo date("Сегодня d.m.Y")."<br>";
 echo date("Этот файл датирован d.mY", filectime(__FILE__));
 ?>
+////////////////////////////////////////////////////////////////
+<?php ## Использование PREG_OFFSET_CAPTURE.
+$st = '<b>жирный текст</b>';
+$re = '|<(\w+).*?>(.*?)</\1|s';
+preg_match($re, $st, $p, PREG_OFFSET_CAPTURE);
+echo "<pre>"; print_r($p); echo "</pre>";
+?>
